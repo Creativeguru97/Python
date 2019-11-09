7fea937e5f19890d44a40ca5038ee7502ab89928
Creativeguru97 <worldapple2630@gmail.com>
2019-11-09 11:19:50 +0900

    Nov 9, 2019

--- Syntax/PythonBasis.py
+++ Syntax/PythonBasis.py
@@ -15,6 +15,8 @@ nsfgnsognosljng
 """
 
 #declare
+#Variables do not need to be declared with any particular
+#type and can even change type after they have been set.
 # N = 5
 # y = "John"
 # print(y)
@@ -26,7 +28,7 @@ nsfgnsognosljng
 # print(z)
 #
 # print("I'd to yeal " + z)
-# #cannot print str and num togather without type change
+#cannot print str and num togather without type change
 # print("Your score is: " + str(N))
 
 # x = 3 + 5j #type: conplex
@@ -36,25 +38,61 @@ nsfgnsognosljng
 # print(type(y))
 # print(type(z))
 
+# import random
+#
+# print(random.randrange(1, 10))
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+#
+# print(a)
+
+# Get the characters from position 2 to position 7 (not included):
 # a = "Hello,world"
 # print(a[2])
 # print(a[2:7])
 
+# Negative indexing
+# b = "Hello,world"
+# print(b[-11:-5])
+
+# a = "Hello,world"
+# print(len(a))
+
+# The strip() method removes any whitespace from the beginning or the end:
+# a = " abcde "
+# print(a)
+# print(a.strip())
 
 #---------------
 #Operators
 #-----------------
+
+"""
+x = 0100111010;
+y = 1001101010;
+z = x|y;
+z == 1101111010;
+"""
+
 # x = 5
-# # x **=3 # x to the power of 3
-# # print=(x)
-# x = x | 3#(x |= 3)
-# """
-# x = 0100111010;
-# y = 1001101010;
-# z = x|y;
-# z == 1101111010;
-# """
+# x **=3 # x to the power of 3
 # print(x)
+
+# x = 5
+# x = x | 3 # Bitwise OR
+# #
+# # 5: in binary 101
+# # 3: in binary 11
+# # So x gonna be 111, which is 7 in decimal number
+# print(x)
+
+# x = 13 # 110 in binary
+# x ^= 12# Bitwise XOR
+# print(x)
+
 
 #List
 # list1 = ["apple", "banana", "cherry"]
@@ -85,11 +123,11 @@ nsfgnsognosljng
 # print(set1)
 
 #Dictionaries
-dict1 = {
-    "brand": "Ford",
-    "model": "Mustang",
-    "year": 1964
-}
+# dict1 = {
+#     "brand": "Ford",
+#     "model": "Mustang",
+#     "year": 1964
+# }
 
 # print(dict1["year"]) # print the value
 # dict1["year"] = 2018
@@ -100,7 +138,7 @@ dict1 = {
 #     print("model exists as one of keys")
 
 #Add items
-dict1["color"] = "blue"
+# dict1["color"] = "blue"
 # print(dict1)
 
 #Remove items
@@ -120,5 +158,5 @@ dict1["color"] = "blue"
 #     print(dict1[x])
 
 #Loop through both keys and values
-for x, y in dict1.items():
-    print(x, y)
+# for x, y in dict1.items():
+#     print(x, y)
